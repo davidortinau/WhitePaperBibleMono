5b6e3fd4eb0dcc2b376373fe60d861af12f68891
Ben Bishop <ben@rendr.io>
2014-02-20 07:11:18 -0600

    Added a null pointer check to fix a RTE.

--- WhitePaperBible.iOS/TableSource/TagsTableSource.cs
+++ WhitePaperBible.iOS/TableSource/TagsTableSource.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Linq;
-
 using WhitePaperBible.Core.Models;
 using System.Collections.Generic;
 using MonoTouch.Dialog;
@@ -9,11 +8,11 @@ using MonoTouch.UIKit;
 
 namespace WhitePaperBible.iOS.TableSource
 {
-	public class TagsTableSource: UITableViewSource {
+	public class TagsTableSource: UITableViewSource
+	{
 		public IList<TagNode> tags;
 		TagsListView view;
-		
-		static NSString cellId = new NSString("TagElement");
+		static NSString cellId = new NSString ("TagElement");
 
 		public TagsTableSource (List<TagNode> tags, TagsListView view)
 		{
@@ -23,24 +22,22 @@ namespace WhitePaperBible.iOS.TableSource
 
 		public override UITableViewCell GetCell (UITableView tableView, MonoTouch.Foundation.NSIndexPath indexPath)
 		{
-			var tagNode = tags[indexPath.Row] as TagNode;
-			var cell = tableView.DequeueReusableCell(cellId);
-			if(cell == null) 
+			var tagNode = tags [indexPath.Row] as TagNode;
+			var cell = tableView.DequeueReusableCell (cellId);
+			if (cell == null)
 				cell = new UITableViewCell (UITableViewCellStyle.Default, cellId);
 			
-			cell.TextLabel.Text = tagNode.tag.ToString();
+			cell.TextLabel.Text = tagNode.tag.ToString ();
 			return cell;
 		}
-		
-//		public override string[] SectionIndexTitles (UITableView tableView)
-//		{
-//			var sit = from node in papers
-//                    group node by (node.paper.title[0].ToString().ToUpper()) into alpha
-//						orderby alpha.Key
-//						select alpha.Key;
-//			return sit.ToArray();
-//		}
-
+		//		public override string[] SectionIndexTitles (UITableView tableView)
+		//		{
+		//			var sit = from node in papers
+		//                    group node by (node.paper.title[0].ToString().ToUpper()) into alpha
+		//						orderby alpha.Key
+		//						select alpha.Key;
+		//			return sit.ToArray();
+		//		}
 		public override float GetHeightForRow (UITableView tableView, NSIndexPath indexPath)
 		{
 			return 60f;
@@ -48,20 +45,18 @@ namespace WhitePaperBible.iOS.TableSource
 
 		public override int RowsInSection (UITableView tableview, int section)
 		{
-			return tags.Count;
+			return (tags != null) ? tags.Count : 0;
 		}
-		
-//		public override string TitleForHeader (UITableView tableView, int section)
-//		{
-//			return "Papers";
-//		}	
-
+		//		public override string TitleForHeader (UITableView tableView, int section)
+		//		{
+		//			return "Papers";
+		//		}
 		public override void RowSelected (UITableView tableView, MonoTouch.Foundation.NSIndexPath indexPath)
 		{
-			var tagNode = tags[indexPath.Row] as TagNode;
-			if (AppDelegate.IsPhone) tableView.DeselectRow (indexPath, true);
+			var tagNode = tags [indexPath.Row] as TagNode;
+			if (AppDelegate.IsPhone)
+				tableView.DeselectRow (indexPath, true);
 		}
-		
 	}
 }
 
