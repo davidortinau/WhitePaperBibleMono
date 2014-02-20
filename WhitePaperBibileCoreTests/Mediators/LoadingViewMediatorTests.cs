7417129b90abdf8ec41642c42cab835bcc2a65d0
Ben Bishop <ben@rendr.io>
2014-02-20 08:25:21 -0600

    Added an example Unit Test with a Mediator using Moq.

---
+++ WhitePaperBibileCoreTests/Mediators/LoadingViewMediatorTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using WhitePaperBible.Core.Mediators;
+using Moq;
+using WhitePaperBible.Core.Views;
+using WhitePaperBible.Core.Invokers;
+using MonkeyArms;
+
+namespace WhitePaperBibileCoreTests
+{
+	[TestFixture ()]
+	public class LoadingViewMediatorTests
+	{
+		[Test (Description = "When PapersReceivedInvoker dispatches, Mediator should call OnLoadingComplete on View")]
+		public void Verify_View_OnLoadingComplete ()
+		{
+			MockPapersReceivedInvoker.Raise (invoker => invoker.Invoked += null, InvokerArgs.Empty);
+			MockView.Verify (view => view.OnLoadingComplete (), Times.Once ());
+		}
+
+		Mock<ILoadingView> MockView;
+		LoadingViewMediator TestMediator;
+		Mock<PapersReceivedInvoker> MockPapersReceivedInvoker;
+
+		[SetUp]
+		public void InitTest ()
+		{
+			MockView = new Mock<ILoadingView> ();
+			MockPapersReceivedInvoker = new Mock<PapersReceivedInvoker> ();
+			TestMediator = new LoadingViewMediator (MockView.Object);
+			TestMediator.PapersReceived = MockPapersReceivedInvoker.Object;
+			TestMediator.Register ();
+		}
+	}
+}
+
