bfb009a43ea0b33640e5e51481660a654f6515ab
David Ortinau <dave@davidortinau.com>
2014-02-22 08:08:31 -0600

    setting up tags tests

---
+++ WhitePaperBible.PCL/Services/GetTagsService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using WhitePaperBible.Core.Models;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace WhitePaperBible.Core.Services
+{
+	public interface IGetTagsService:IBaseService
+	{
+		void Execute ();
+	}
+
+	public class GetTagsService:BaseService, IGetTagsService
+	{
+		public GetTagsService ()
+		{
+
+		}
+
+		public GetTagsService (IJSONWebClient webClient) : base (webClient)
+		{
+	
+		}
+
+		public void Execute ()
+		{
+			Client.OpenURL (Constants.BASE_URI + "tag.json?caller=wpb-iPhone");
+		}
+
+		#region implemented abstract members of BaseService
+
+		protected override void HandleSuccess (object sender, EventArgs args)
+		{
+			DispatchSuccess (new GetTagsServiceEventArgs (ParseResponse<List<TagNode>> ()));
+		}
+
+		#endregion
+	}
+
+	public class GetTagsServiceEventArgs:EventArgs
+	{
+		public readonly List<TagNode> Tags;
+
+		public GetTagsServiceEventArgs (List<TagNode> tags)
+		{
+			Tags = tags;
+		}
+	}
+}
+
