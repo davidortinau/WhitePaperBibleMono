097602373dd7fc2fc699a09fdd92309cb4ea7dc1
Ben Bishop <ben@rendr.io>
2014-03-02 12:29:29 -0600

    Reworked the Login/Account Requirement.
    Created a new view that explains to the user they need to have an account to use that particular feature. This allows the user to cancel out of Login and be able to reopen login form after canceling.

--- WhitePaperBible.iOS/Views/iPhone/FavoritesView.cs
+++ WhitePaperBible.iOS/Views/iPhone/FavoritesView.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 using MonoTouch.Dialog;
@@ -14,12 +13,22 @@ namespace WhitePaperBible.iOS
 {
 	public partial class FavoritesView : DialogViewController, IFavoritesView
 	{
+		LoginRequiredView LoginRequiredView;
+
 		public void PromptForLogin ()
 		{
+			if (LoginRequiredView == null) {
+				CreateLoginRequiredView ();
+				LoginRequiredView.Hidden = false;
+			}
+		}
+
+		public void ShowLoginForm ()
+		{
 			var loginView = new LoginView ();
-//			loginView.LoginFinished.Invoked += HandleLoginFinished;
+			//			loginView.LoginFinished.Invoked += HandleLoginFinished;
 			loginView.LoginFinished.Invoked += (object sender, EventArgs e) => {
-				(e as LoginFinishedInvokerArgs).Controller.DismissViewController(true, null);
+				(e as LoginFinishedInvokerArgs).Controller.DismissViewController (true, null);
 			};
 
 			this.PresentViewController (loginView, true, null);
@@ -32,6 +41,8 @@ namespace WhitePaperBible.iOS
 			SearchPlaceholder = @"Find Papers";
 			this.Filter = new Invoker ();
 			this.OnPaperSelected = new Invoker ();
+
+
 		}
 
 		#region IPapersListView implementation
@@ -92,6 +103,14 @@ namespace WhitePaperBible.iOS
 				Console.WriteLine ("search text changed");	
 			};
 
+
+
+		}
+
+		public override void ViewDidAppear (bool animated)
+		{
+			base.ViewDidAppear (animated);
+			View.BringSubviewToFront (LoginRequiredView);
 		}
 
 		public override void ViewDidDisappear (bool animated)
@@ -99,6 +118,15 @@ namespace WhitePaperBible.iOS
 			base.ViewDidDisappear (animated);
 
 			DI.DestroyMediator (this);
+		}
+
+		protected void CreateLoginRequiredView ()
+		{
+			LoginRequiredView = new LoginRequiredView (WhitePaperBible.iOS.UI.Environment.DeviceScreenHeight);
+			View.AddSubview (LoginRequiredView);
+			View.BringSubviewToFront (LoginRequiredView);
+			LoginRequiredView.LoginRegister.Invoked += (object sender, EventArgs e) => ShowLoginForm ();
+			LoginRequiredView.Hidden = true;
 		}
 	}
 }
---
+++ WhitePaperBible.iOS/Views/iPhone/Sub/LoginRequiredView.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using MonoTouch.UIKit;
+using System.Drawing;
+using WhitePaperBible.iOS.UI;
+using WhitePaperBible.iOS.Managers;
+using MonkeyArms;
+
+namespace WhitePaperBible.iOS
+{
+	public class LoginRequiredView:UIView
+	{
+		public readonly Invoker LoginRegister = new Invoker ();
+
+		public LoginRequiredView (float height) : base (new RectangleF (0, 0, 320, height))
+		{
+			this.BackgroundColor = AppStyles.OffWhite;
+		}
+
+		public override void LayoutSubviews ()
+		{
+			base.LayoutSubviews ();
+			CreateDescription ();
+			CreateLoginRegisterButton ();
+
+		}
+
+		void CreateLoginRegisterButton ()
+		{
+			WPBButton loginRegisterButton = new WPBButton (ResourceManager.GetString ("loginRegister"),
+				                                AppStyles.Red,
+				                                130);
+			loginRegisterButton.TouchUpInside += (object sender, EventArgs e) => {
+				LoginRegister.Invoke ();
+			};
+			AddSubview (loginRegisterButton);
+		}
+
+		void CreateDescription ()
+		{
+			UITextView description = new UITextView (new RectangleF (0, 50, Frame.Width, Frame.Height));
+			description.Text = ResourceManager.GetString ("loginRequired");
+			description.BackgroundColor = UIColor.Clear;
+			description.Font = AppStyles.HelveticaNeue (20);
+			description.TextAlignment = UITextAlignment.Center;
+			description.Editable = false;
+			AddSubview (description);
+		}
+	}
+}
+
