d7e480f68a9e6dc18e91d8e05108e54437bb0e07
Ben Bishop <ben@rendr.io>
2014-03-06 08:05:55 -0600

    Updated MonkeyArms to latest version (with InjectionMap)

---
+++ WhitePaperBible.PCL/Services/AuthenticateUserService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ServiceModel.Security;
+
+namespace WhitePaperBible.Core.Services
+{
+	public class AuthenticateUserService:BaseService
+	{
+		public void Execute (string username, string password)
+		{
+
+		}
+
+		#region implemented abstract members of BaseService
+
+		protected override void HandleSuccess (object sender, EventArgs args)
+		{
+
+		}
+
+		#endregion
+	}
+}
+
