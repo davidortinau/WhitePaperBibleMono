9bcce967e41d0f2f2baddf20d94da9b996378d3f
David Ortinau <dave@rendr.io>
2015-09-18 22:19:42 -0500

    added action bar, refactored base, replaced tabs
    updated packages

---
+++ WhitePaperBible.Android/BaseActivityView.cs
@@ -0,0 +1,132 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using MonkeyArms;
+using Android.Graphics.Drawables;
+
+namespace WhitePaperBible.Droid
+{
+	[Activity]			
+	public class BaseActivityView : AppCompatActivity, IMediatorTarget
+	{
+//		private int _layoutId;
+//
+//		public BaseActivityView (int layoutId)
+//		{
+//			_layoutId = layoutId;
+//		}
+//
+//		protected override void OnCreate(Bundle bundle)
+//		{			
+//			RequestWindowFeature(WindowFeatures.NoTitle);
+//			base.OnCreate(bundle);
+//			SetContentView(_layoutId);  
+//			configureActionBar (Resource.Id.toolbar);
+//		}
+
+		private void configureActionBar(int idResource)
+		{
+			var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar> (idResource);
+			if (toolbar != null) {
+				SetSupportActionBar (toolbar);
+				setSupportActionBarTitle ("");
+			}
+		}
+
+		protected void OnCreateWithLayout(Bundle bundle, int layoutId)
+		{
+			RequestWindowFeature(WindowFeatures.NoTitle);
+
+			base.OnCreate(bundle);
+			SetContentView(layoutId);
+			configureActionBar (Resource.Id.toolbar);
+		}
+
+//		public override bool OnCreateOptionsMenu(IMenu menu)
+//		{
+//			if (_hideOverflow)
+//			{
+//				MenuInflater.Inflate(Resource.Menu.empty, menu);
+//			}
+//			else
+//			{
+//				MenuInflater.Inflate(Resource.Menu.home, menu);
+//			}
+//			return base.OnCreateOptionsMenu(menu);
+//		}
+
+		public override bool OnOptionsItemSelected(IMenuItem item)
+		{
+			return base.OnOptionsItemSelected(item);
+		}
+
+		protected override void OnResume()
+		{
+			base.OnResume();
+			try
+			{
+				DI.RequestMediator(this);
+			}
+			catch
+			{
+				Console.WriteLine("Unable to request mediator for: " + this.Class.Name);
+			}
+		}
+
+		protected override void OnPause()
+		{
+			base.OnPause();
+			try
+			{
+				DI.DestroyMediator(this);
+			}
+			catch
+			{
+				Console.WriteLine("Unable to destroy mediator for: " + this.Class.Name);
+			}
+		}
+
+		protected void Logout()
+		{
+//			Intent intent = new Intent(this, typeof(LoginActivity));
+//			intent.AddFlags(ActivityFlags.ClearTask);
+//			intent.AddFlags(ActivityFlags.NewTask);
+//			StartActivity(intent);
+		}
+
+		protected void showToast(int resourceId)
+		{
+			var toast = Toast.MakeText(this,resourceId, ToastLength.Short);
+			toast.Show();
+		}
+
+		protected void setSupportActionBarTitle(string title)
+		{
+			this.SupportActionBar.Title = title; 
+		}
+		protected void setSupportActionBarTitle(int resourceId)
+		{
+			this.setSupportActionBarTitle (Resources.GetString (resourceId));
+		}
+		protected void setSupportActionBarBackgroundColor(int idResourceColor)
+		{
+			this.SupportActionBar.SetBackgroundDrawable (new ColorDrawable (Resources.GetColor(idResourceColor)));
+		}
+		protected void addSupportActionBarBackButton()
+		{			
+			this.SupportActionBar.SetDisplayHomeAsUpEnabled (true);
+		}
+	}
+}
+
---
+++ WhitePaperBible.Android/MainView.cs
@@ -0,0 +1,57 @@
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+
+using WhitePaperBible.Core.Views;
+using MonkeyArms;
+using WhitePaperBible.Droid.Fragments;
+using Android.Support.V4.App;
+using Android.Views;
+using Android.Support.V4.View;
+using Adapters;
+using com.refractored;
+using Android.Support.Design.Widget;
+
+namespace WhitePaperBible.Droid
+{
+	[Activity (MainLauncher=true, NoHistory=true)]			
+	public class MainView : BaseActivityView, ILoadingView
+	{
+		protected BaseFragment CurrentScreen;
+
+		protected override void OnStart ()
+		{
+			base.OnStart ();
+		}
+
+		protected override void OnCreate (Bundle bundle)
+		{
+//			RequestWindowFeature(WindowFeatures.NoTitle);
+
+			base.OnCreateWithLayout(bundle, Resource.Layout.MainView);
+
+			var pager = FindViewById<ViewPager>(Resource.Id.pager);
+			pager.Adapter = new MainViewPagerAdapter(this.SupportFragmentManager, this.Resources);
+
+			var tabs = FindViewById<TabLayout>(Resource.Id.tab_layout);    
+			tabs.SetupWithViewPager (pager);            
+
+			this.setSupportActionBarTitle (Resource.String.tab_papers);
+			tabs.TabGravity=TabLayout.GravityFill;
+
+//			DI.RequestMediator(this);
+		}
+
+		#region ILoadingView implementation
+		public void OnLoadingComplete ()
+		{
+			var papersView = new Intent(this, typeof(PapersView));
+			StartActivity( papersView );
+		}
+		#endregion
+
+	}
+}
+
+
